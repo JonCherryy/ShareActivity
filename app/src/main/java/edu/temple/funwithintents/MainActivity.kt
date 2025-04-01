5bce5a17461c9609828f4da8c87a0d39e042419a
Jonathan Cherry <tuo97200@temple.edu>
2025-04-01 12:06:05 -0400

    Done with intents

--- app/src/main/java/edu/temple/funwithintents/MainActivity.kt
+++ app/src/main/java/edu/temple/funwithintents/MainActivity.kt
@@ -1,8 +1,10 @@
 package edu.temple.funwithintents
 
+import android.content.Intent
 import android.os.Bundle
 import android.widget.EditText
 import android.widget.ImageButton
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
@@ -15,7 +17,14 @@ class MainActivity : AppCompatActivity() {
 
         // When the user clicks this button, share the text if not empty
         findViewById<ImageButton>(R.id.shareImageButton).setOnClickListener {
-
+            val text = editText.toString()
+            if(text.isNotEmpty()){
+                val shareIntent = Intent(Intent.ACTION_VIEW).apply{
+                    type = "text/plain"
+                    putExtra(Intent.EXTRA_TEXT,text)
+                }
+                startActivity(Intent.createChooser(shareIntent, "Share"))
+            }
         }
     }
 }
